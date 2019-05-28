98a212ebd2e9de78c66bd38f8b1ee753c3944165
Jeroen Claassens <j.claassens@cgi.com>
2019-05-28 12:45:04 +0200

    Fix example running, sample button

--- ui-components/example/src/App.js
+++ ui-components/example/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'ui-components'
+import ExampleComponent from '@rws-air/ui-components'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <ExampleComponent label='Modern React component module' onClick={() => console.log('vbll')} />
       </div>
     )
   }
--- ui-components/src/index.tsx
+++ ui-components/src/index.tsx
@@ -3,21 +3,48 @@
  */
 
 import * as React from 'react'
-
-import styles from './styles.scss'
+import classnames from 'classnames';
+import css from './Button.scss';
+import { Button as MUIButton } from '@material-ui/core';
+import { ButtonProps } from '@material-ui/core/Button';
 
 export type Props = { text: string }
+export type RWSButtonProps = {
+  label: React.ReactNode;
+  disabled?: boolean;
+  customClasses?: string | string[];
 
-export default class ExampleComponent extends React.Component<Props> {
-  render() {
-    const {
-      text
-    } = this.props
+  onClick (): any;
+};
 
+export default class Button extends React.Component<ButtonProps<'button', RWSButtonProps>> {
+  render () {
     return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
+      <MUIButton
+      onClick={this.props.onClick}
+      variant={this.props.variant}
+      color={this.props.color}
+      disabled={this.props.disabled}
+      className={classnames(this.props.customClasses)}
+      classes={{root: css.rwsButton, label: css.rwsButtonLabel}}
+      href =''
+      >
+        {this.props.label}
+      </MUIButton>
     )
   }
 }
+
+// export default class ExampleComponent extends React.Component<Props> {
+//   render() {
+//     const {
+//       text
+//     } = this.props
+
+//     return (
+//       <div>
+//         Example Component: {text}
+//       </div>
+//     )
+//   }
+// }
