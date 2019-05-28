edc567efd6654b7d58fb6f893a03f07bb6a12fe7
Jeroen Claassens <j.claassens@cgi.com>
2019-05-28 22:05:53 +0200

    (webcomponents) Utilize react-scripts for react-styleguidist

--- webcomponents/src/typings.d.ts
+++ webcomponents/src/typings.d.ts
@@ -1,7 +1,3 @@
-/**
- * Default CSS definition for typescript,
- * will be overridden with file-specific definitions by rollup
- */
 declare module '*.css' {
   const content: { [className: string]: string };
   export default content;
