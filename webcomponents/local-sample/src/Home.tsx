d827e011204c9224053881417c5b4a4894da44b2
Jeroen Claassens <j.claassens@cgi.com>
2019-06-04 17:12:14 +0200

    Better display of actionbar on local sample

--- webcomponents/local-sample/src/Home.tsx
+++ webcomponents/local-sample/src/Home.tsx
@@ -1,17 +1,21 @@
-import React, { FC } from 'react';
+import React, { FC, Fragment } from 'react';
 import css from 'styles/modules/app.module.scss';
 
 import { Button, Logo, ActionBar } from '@rws-air/ui-components'
 
 const Home: FC = () => {
   return (
-    <div className={css.app}>
-      <header className={css.header}>
+    <Fragment>
+      <div>
         <ActionBar title='ActionBar Title' buttonLabel='action-bar-button' buttonAction={() => console.log('test')} shouldHaveButton />
-        <Button variant='contained' color='primary' label='button' onClick={() => console.log('void')} />
-        <Logo />
-      </header>
-    </div>
+      </div>
+      <div className={css.app}>
+        <header className={css.header}>
+          <Button variant='contained' color='primary' label='button' onClick={() => console.log('void')} />
+          <Logo />
+        </header>
+      </div>
+    </Fragment>
   );
 }
 
