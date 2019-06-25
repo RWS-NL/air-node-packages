b1e57f8273ea01a2386535139a5638582ffcb865
Jeroen Claassens <j.claassens@cgi.com>
2019-06-25 11:34:03 +0200

    (webcomponents) Fixed styling for buttons

--- webcomponents/local-sample/src/Home.tsx
+++ webcomponents/local-sample/src/Home.tsx
@@ -9,10 +9,15 @@ const Home: FC = () => {
       <div>
         <ActionBar title='ActionBar Title' buttonLabel='action-bar-button' buttonAction={() => console.log('test')} shouldHaveButton />
       </div>
+      <div className={css.logo}>
+        <Logo />
+      </div>
       <div className={css.app}>
         <header className={css.header}>
-          <Button variant='contained' color='primary' label='button' onClick={() => console.log('void')} />
-          <Logo />
+          <Button variant='contained' color='primary' label='Contained Primary Button' onClick={() => console.log('Clicked Contained Primary Button')} />
+          <Button variant='contained' color='secondary' label='Contained Secondary Button' onClick={() => console.log('Clicked Contained Secondary Button')} />
+          <Button variant='outlined' color='primary' label='Outlined Primary Button' onClick={() => console.log('Clicked Outlined Primary Button')} />
+          <Button variant='outlined' color='secondary' label='Outlined Secondary Button' onClick={() => console.log('Clicked Outlined Secondary Button')} />
         </header>
       </div>
     </Fragment>
