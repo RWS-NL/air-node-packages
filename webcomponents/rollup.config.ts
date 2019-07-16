6045cdb1747bfb14a8232428cc5b218350f1280c
Jeroen Claassens <j.claassens@cgi.com>
2019-07-16 10:42:19 +0200

    TSLint fixes

--- webcomponents/rollup.config.ts
+++ webcomponents/rollup.config.ts
@@ -1,11 +1,11 @@
-import typescript from 'rollup-plugin-typescript2';
+import svgr from '@svgr/rollup';
 import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
 import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
-import resolve from 'rollup-plugin-node-resolve';
 import progress from 'rollup-plugin-progress';
+import typescript from 'rollup-plugin-typescript2';
 import url from 'rollup-plugin-url';
-import svgr from '@svgr/rollup';
 
 export default {
   input: 'src/index.ts',
@@ -14,13 +14,13 @@ export default {
       file: './dist/index.js',
       format: 'cjs',
       exports: 'named',
-      sourcemap: true
+      sourcemap: true,
     },
     {
       file: './dist/index.es.js',
       format: 'es',
       exports: 'named',
-      sourcemap: true
+      sourcemap: true,
     }
   ],
   plugins: [
@@ -28,15 +28,15 @@ export default {
     external(),
     postcss({
       modules: true,
-      minimize: true
+      minimize: true,
     }),
     url(),
     svgr(),
     resolve(),
     typescript({
       rollupCommonJSResolveHack: true,
-      clean: true
+      clean: true,
     }),
     commonjs()
-  ]
-}
+  ],
+};
