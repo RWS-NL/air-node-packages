1e1b30ce51d8b0d6f8e5d2ed40bfa352dafd8ee1
Jeroen Claassens <j.claassens@cgi.com>
2019-08-06 21:18:21 +0200

    Bootstrap Lerna

--- packages/eslint-config/rollup.config.ts
+++ packages/eslint-config/rollup.config.ts
@@ -1,13 +1,10 @@
-import svgr from '@svgr/rollup';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import external from 'rollup-plugin-peer-deps-external';
-import postcss from 'rollup-plugin-postcss';
 import progress from 'rollup-plugin-progress';
 import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 import cleaner from 'rollup-plugin-cleaner';
-import url from 'rollup-plugin-url';
 
 export default {
   input: 'src/index.ts',
@@ -33,18 +30,12 @@ export default {
     }),
     progress(),
     external(),
-    postcss({
-      modules: true,
-      minimize: true,
-    }),
-    url(),
-    svgr(),
     resolve(),
     typescript({
       rollupCommonJSResolveHack: true,
       clean: true,
     }),
     commonjs(),
-    terser({ ecma: 5 }),
+    terser({ ecma: 5 })
   ],
 };
--- packages/stylelint-config/rollup.config.ts
+++ packages/stylelint-config/rollup.config.ts
@@ -4,6 +4,7 @@ import external from 'rollup-plugin-peer-deps-external';
 import progress from 'rollup-plugin-progress';
 import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
+import cleaner from 'rollup-plugin-cleaner';
 
 export default {
   input: 'src/index.ts',
@@ -16,6 +17,11 @@ export default {
     }
   ],
   plugins: [
+    cleaner({
+      targets: [
+        './dist/'
+      ],
+    }),
     progress(),
     external(),
     resolve(),
