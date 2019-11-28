115af30228f371b9bd6726c1fae39c4cda3da6d5
Jeroen Claassens <j.claassens@cgi.com>
2019-11-28 15:21:57 +0100

    build(dev-deps): bump versions

--- packages/webcomponents/rollup.config.js
+++ packages/webcomponents/rollup.config.js
@@ -1,6 +1,6 @@
 import configure from '../../rollup.config';
 import svgr from '@svgr/rollup';
-import url from 'rollup-plugin-url';
+import url from '@rollup/plugin-url';
 import postcss from 'rollup-plugin-postcss';
 
 export default configure({
