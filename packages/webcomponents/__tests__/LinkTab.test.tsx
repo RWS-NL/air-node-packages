53fa5dd509c4ab550567b8735e28aed15e3a826e
Gerard Mulder <gerard.mulder@rws.nl>
2020-01-23 14:37:01 +0100

    feat(webcomponents): added LinkTab and NavBar to webcomponents
    [#168927565]
    
    Signed-off-by: Jeroen Claassens <j.claassens@cgi.com>

---
+++ packages/webcomponents/__tests__/LinkTab.test.tsx
@@ -0,0 +1,22 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import LinkTab from '../src/LinkTab';
+
+describe('LinkTab Tests', function() {
+  describe('Snapshot Testing', () => {
+    test('Required Props', () => {
+      const linkTab = shallow(<LinkTab to='/root' label='sample' />);
+      expect(linkTab).toMatchSnapshot();
+    });
+
+    test('External Tab', () => {
+      const linkTab = shallow(<LinkTab to='https://cgi.com' label='sample' external />);
+      expect(linkTab).toMatchSnapshot();
+    });
+
+    test('External Tab with Open In New Tab', () => {
+      const linkTab = shallow(<LinkTab to='https://rijkswaterstaat.nl' label='sample' external openInNewTab />);
+      expect(linkTab).toMatchSnapshot();
+    });
+  });
+});
---
+++ packages/webcomponents/src/LinkTab/index.tsx
@@ -0,0 +1,52 @@
+import React, { memo } from 'react';
+import { Tab, TabProps } from '@material-ui/core';
+
+import css from './LinkTab.scss';
+import { Link } from 'react-router-dom';
+
+export interface LinkTabProps {
+  /** The label to show in the tab */
+  label: string;
+  /** The URL for this tab to navigate to */
+  to: string;
+  /**
+   * Whether this link should go to an external site or not, using an `<a href=...>` component
+   * @default false
+   */
+  external?: boolean;
+  /**
+   * Whether this link should open in a new tab or not
+   * Only applies when external = true
+   * @default false
+   */
+  openInNewTab?: boolean;
+
+  /** Additional props to pass to each Tab component */
+  TabProps?: TabProps;
+}
+
+export const LinkTab = memo(({ to, label, external = false, openInNewTab = false, ...props }: LinkTabProps) => {
+  if (external) {
+    return (
+      <Tab
+        {...props}
+        component='a'
+        href={to}
+        target={openInNewTab ? '_blank' : '_self'}
+        rel={openInNewTab ? 'noreferrer noopener' : undefined}
+        classes={{ wrapper: css.linkText, selected: css.selectedTab, textColorInherit: css.linkTextInherit }}
+      />
+    );
+  }
+
+  return (
+    <Tab
+      {...props}
+      component={Link}
+      to={to}
+      classes={{ wrapper: css.linkText, selected: css.selectedTab, textColorInherit: css.linkTextInherit }}
+    />
+  );
+});
+
+export default LinkTab;
