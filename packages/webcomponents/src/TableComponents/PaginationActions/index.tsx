7b55fb20d5ba1acc0fb163b8e248ed0b9ddd712c
Jeroen Claassens <j.claassens@cgi.com>
2020-02-13 10:59:53 +0100

    refactor(webcomponents): clean duplicate code from PaginationActions
    [#170893251]
    
    Signed-off-by: Jeroen Claassens <j.claassens@cgi.com>

--- packages/webcomponents/src/TableComponents/PaginationActions/index.tsx
+++ packages/webcomponents/src/TableComponents/PaginationActions/index.tsx
@@ -1,6 +1,5 @@
 import Box from '@material-ui/core/Box';
 import Chip from '@material-ui/core/Chip';
-import IconButton from '@material-ui/core/IconButton';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import FirstPageIcon from '@material-ui/icons/FirstPage';
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
@@ -8,7 +7,9 @@ import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import LastPageIcon from '@material-ui/icons/LastPage';
 import classnames from 'classnames';
 import React, { memo, SyntheticEvent } from 'react';
+import { Else, If, Then } from 'react-if';
 import css from './PaginationActions.scss';
+import PaginationButton, { PaginationButtonProps } from './PaginationButton';
 
 export interface PaginationActionsProps {
   /** Amount of rows per page */
@@ -29,84 +30,88 @@ export interface PaginationActionsProps {
  * Constructs a table pagination action navigators using pre-defined Rijkswaterstaat styling
  * @param props Props to pass to the table pagination
  */
-export const PaginationActions = memo((props: PaginationActionsProps) => {
-  const handleFirstPageButtonclick = (event: any) => props.onChangePage(event, 0);
-  const handleBackButtonClick = (event: any) => props.onChangePage(event, props.page - 1);
-  const handleNextButtonClick = (event: any) => props.onChangePage(event, props.page + 1);
-  const handleLastPageButtonClick = (event: any) =>
-    props.onChangePage(event, Math.max(0, Math.ceil(props.count / props.rowsPerPage) - 1));
-  const handlePageClick = (page: number) => (event: SyntheticEvent) => props.onChangePage(event, page);
-  const isOnMobile = useMediaQuery('(max-width: 1024px)');
+export const PaginationActions = memo(
+  ({ onChangePage, count, rowsPerPage, page, 'data-qa': dataQa }: PaginationActionsProps) => {
+    const handleFirstPageButtonclick = <T extends unknown>(event: T): void => onChangePage(event, 0);
+    const handleBackButtonClick = <T extends unknown>(event: T): void => onChangePage(event, page - 1);
+    const handleNextButtonClick = <T extends unknown>(event: T): void => onChangePage(event, page + 1);
+    const handleLastPageButtonClick = <T extends unknown>(event: T): void =>
+      onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+    const handlePageClick = (page: number) => (event: SyntheticEvent): void => onChangePage(event, page);
 
-  const renderCurrentPage = (
-    key: number,
-    page: number,
-    clickEvent: (page: number) => (event: React.SyntheticEvent<Element, Event>) => void
-  ): JSX.Element => {
+    const renderCurrentPage = (
+      key: number,
+      page: number,
+      clickEvent: (page: number) => (event: React.SyntheticEvent<Element, Event>) => void
+    ): JSX.Element => {
+      return (
+        <Chip
+          key={key}
+          label={page}
+          color='primary'
+          variant='default'
+          onClick={clickEvent(page - 1)}
+          className={classnames(css.activePageChip, css.ie11ChipCorrection)}
+        />
+      );
+    };
+
+    const renderPages = () =>
+      [...Array(Math.ceil(count / rowsPerPage)).keys()]
+        .map(x => ++x)
+        .map((page, key) => {
+          if (page + 1 === page) {
+            return renderCurrentPage(key, page, handlePageClick);
+          }
+
+          return (
+            <Box
+              component='span'
+              key={key}
+              onClick={handlePageClick(page - 1)}
+              className={classnames(css.paginationNumbers)}
+            >
+              {page}
+            </Box>
+          );
+        });
+
+    const isOnMobile = useMediaQuery('(max-width: 1024px)');
+    const nextButtonShouldBeDisabled = page >= Math.ceil(count / rowsPerPage) - 1;
+    const previousButtonShouldBeDisabled = page === 0;
+
     return (
-      <Chip
-        key={key}
-        label={page}
-        color='primary'
-        variant='default'
-        onClick={clickEvent(page - 1)}
-        className={classnames(css.activePageChip, css.ie11ChipCorrection)}
-      />
+      <Box style={{ display: 'flex', alignItems: 'center' }} data-qa={dataQa}>
+        <PaginationButton
+          onClick={handleFirstPageButtonclick}
+          disabled={previousButtonShouldBeDisabled}
+          icon={<FirstPageIcon />}
+        />
+        <PaginationButton
+          onClick={handleBackButtonClick}
+          disabled={previousButtonShouldBeDisabled}
+          icon={<KeyboardArrowLeft />}
+        />
+
+        <If condition={isOnMobile}>
+          <Then>{renderCurrentPage(1, page + 1, handlePageClick)}</Then>
+          <Else>{renderPages()}</Else>
+        </If>
+
+        <PaginationButton
+          onClick={handleNextButtonClick}
+          disabled={nextButtonShouldBeDisabled}
+          icon={<KeyboardArrowRight />}
+        />
+        <PaginationButton
+          onClick={handleLastPageButtonClick}
+          disabled={nextButtonShouldBeDisabled}
+          icon={<LastPageIcon />}
+        />
+      </Box>
     );
-  };
+  }
+);
 
-  const renderPages = () =>
-    [...Array(Math.ceil(props.count / props.rowsPerPage)).keys()]
-      .map(x => ++x)
-      .map((page, key) => {
-        if (props.page + 1 === page) {
-          return renderCurrentPage(key, page, handlePageClick);
-        }
-
-        return (
-          <span key={key} onClick={handlePageClick(page - 1)} className={classnames(css.paginationNumbers)}>
-            {page}
-          </span>
-        );
-      });
-
-  return (
-    <Box style={{ display: 'flex', alignItems: 'center' }} data-qa={props['data-qa']}>
-      <IconButton
-        onClick={handleFirstPageButtonclick}
-        disabled={props.page === 0}
-        color='primary'
-        className={css.noHoverBackground}
-      >
-        <FirstPageIcon />
-      </IconButton>
-      <IconButton
-        onClick={handleBackButtonClick}
-        disabled={props.page === 0}
-        color='primary'
-        className={css.noHoverBackground}
-      >
-        <KeyboardArrowLeft />
-      </IconButton>
-      {!isOnMobile ? renderPages() : renderCurrentPage(1, props.page + 1, handlePageClick)}
-      <IconButton
-        onClick={handleNextButtonClick}
-        disabled={props.page >= Math.ceil(props.count / props.rowsPerPage) - 1}
-        color='primary'
-        className={css.noHoverBackground}
-      >
-        <KeyboardArrowRight />
-      </IconButton>
-      <IconButton
-        onClick={handleLastPageButtonClick}
-        disabled={props.page >= Math.ceil(props.count / props.rowsPerPage) - 1}
-        color='primary'
-        className={css.noHoverBackground}
-      >
-        <LastPageIcon />
-      </IconButton>
-    </Box>
-  );
-});
-
+export { PaginationButtonProps, PaginationButton };
 export default PaginationActions;
