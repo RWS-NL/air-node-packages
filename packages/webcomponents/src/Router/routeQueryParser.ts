865e8cad8c723c4405c90d71289fd7bbc06b9cc4
Jeroen Claassens <j.claassens@cgi.com>
2020-05-07 15:47:08 +0200

    fix(webcomponents): Fix RouteQueryParser
    There was a breaking change in @favware/querystring, this fixes the routeQueryParser to address  that.

---
+++ packages/webcomponents/src/Router/routeQueryParser.ts
@@ -0,0 +1,15 @@
+import stringify from '@favware/querystring';
+
+/**
+ * Safely stringifies queryparameters and prefixes a `?`.
+ * Applies URL encoding with `encodeURIComponent()`.
+ * In case an empty object was provided an empty string is returned
+ * @param params Queryparams to stringify
+ * @returns Stringified params or empty string
+ * @see [`@favware/querystring`](https://favware.tech/querystring)
+ */
+export const routeQueryParser = <T>(params: T) => {
+  const encodedUriParams = stringify(params);
+
+  return encodedUriParams ? `?${encodedUriParams}` : '';
+};
