f281c68b85d5fab55265d742336417623a83e16d
Jeroen Claassens <j.claassens@cgi.com>
2020-05-08 17:59:41 +0200

    refactor(utils): infer type of objects put into mergeobjects

--- packages/utils/src/MergeObjects.ts
+++ packages/utils/src/MergeObjects.ts
@@ -7,17 +7,17 @@ export type KeyedObject = Record<PropertyKey, unknown>;
  * @param objTarget The object to be merged
  * @param objSource The object to merge
  */
-export function mergeObjects<A extends KeyedObject, B extends KeyedObject>(objTarget: A, objSource: B): A & B {
+export function mergeObjects<A, B>(objTarget: A, objSource: B): A & B {
   for (const [key, value] of Object.entries(objSource) as [keyof B, unknown][]) {
-    const targetValue = objTarget[key];
+    const targetValue = (objTarget as KeyedObject)[key];
     if (isObject(value)) {
       Reflect.set(
-        objTarget,
+        objTarget as KeyedObject,
         key,
         isObject(targetValue) ? mergeObjects(targetValue as KeyedObject, value as KeyedObject) : value
       );
     } else if (!isObject(targetValue)) {
-      Reflect.set(objTarget, key, value);
+      Reflect.set(objTarget as KeyedObject, key, value);
     }
   }
   return objTarget as A & B;
