f2140d7edf139964d44e60f4e6b2cc8af0bec5b7
Jeroen Claassens <j.claassens@cgi.com>
2020-05-25 08:38:36 +0200

    feat(utils): deprecate objHasProperty
    objHasProperty can be 1:1 replaced with the more consistent and better "Reflect.has()" syntax from
    the Reflect API. The function will be removed in a future major version.

--- packages/utils/src/ObjectHasProperty.ts
+++ packages/utils/src/ObjectHasProperty.ts
@@ -1,8 +1,18 @@
+import { deprecate } from 'util';
+
+function objHasProp<O>(obj: O, prop: keyof O) {
+  return obj && prop in obj;
+}
+
 /**
  * Type safely checks if an object includes a given property without using the prototype
  * @param obj Object to analyze
  * @param prop Property to check for
+ * @deprecated use Reflect API instead
+ * @example Reflect.has(obj, prop)
+ * @example objectHasProperty(obj, prop)
  */
-export function objectHasProperty<O extends Record<PropertyKey, unknown>>(obj: O, prop: keyof O) {
-  return obj && prop in obj;
-}
+export const objectHasProperty = deprecate(
+  (...args: Parameters<typeof objHasProp>) => objHasProp(args[0], args[1]),
+  'objectHasProperty is deprecated, use Reflect API with "Reflect.has(obj, \'prop\')" instead.'
+);
