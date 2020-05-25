4f8c41d2c2d7e7f5e4f213d131e539df51d34968
Jeroen Claassens <j.claassens@cgi.com>
2020-05-25 08:46:33 +0200

    fix(*): fix yarn resolutions breaking and allowing insecure transient dependencies

--- packages/utils/__tests__/ObjectHasProperty.test.ts
+++ packages/utils/__tests__/ObjectHasProperty.test.ts
@@ -2,6 +2,7 @@ import { objectHasProperty } from '../src/ObjectHasProperty';
 
 const obj = { key: 'value' };
 test('should contain property', () => {
+  // @ts-expect-error
   expect(objectHasProperty(obj, 'key')).toStrictEqual(true);
 });
 
