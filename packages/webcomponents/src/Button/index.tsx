fa7cc0b66f93470a1e2de5991aae17e20121f7dd
Jeroen Claassens <j.claassens@cgi.com>
2020-06-11 14:21:04 +0200

    fix(webcomponents): added a forwardref to button to allow tooltip wrapping

--- packages/webcomponents/src/Button/index.tsx
+++ packages/webcomponents/src/Button/index.tsx
@@ -1,7 +1,7 @@
 import { createStyles, makeStyles, Theme } from '@material-ui/core';
 import MUIButton, { ButtonProps as MUIButtonProps } from '@material-ui/core/Button';
 import clsx from 'clsx';
-import React, { memo, ReactNode } from 'react';
+import React, { forwardRef, memo, ReactNode } from 'react';
 import css from './Button.scss';
 
 export interface ButtonProps {
@@ -29,37 +29,42 @@ const useStyles = makeStyles((theme: Theme) =>
 
 /**
  * Constructs a button using pre-defined Rijkswaterstaat styling
+ * @remark Ref is forwarded to the Material-UI button to allow for wrapping this button with a Tooltip
  * @param props Props to pass to the button
  * @example
  * ```jsx
  * <Button label='button' onClick={() => undefined} variant='contained' color='primary' />
  * ```
  */
-export const Button = memo((props: MUIButtonProps<'button', ButtonProps>) => {
-  const classes = useStyles();
 
-  return (
-    <MUIButton
-      {...props}
-      data-qa={props['data-qa']}
-      onClick={props.onClick}
-      variant={props.variant}
-      color={props.color}
-      disabled={props.disabled}
-      className={clsx(props.customclasses)}
-      classes={{
-        root: css.button,
-        label: clsx(css.buttonLabel, props.customlabelclasses),
-        disabled: css.buttonDisabled,
-        containedPrimary: clsx(css.buttonPrimary, classes.buttonShadow),
-        containedSecondary: clsx(css.buttonSecondary, classes.buttonShadow),
-        outlined: clsx(css.buttonOutlined, classes.buttonShadow),
-        outlinedPrimary: clsx(css.buttonOutlined, classes.buttonShadow),
-        outlinedSecondary: clsx(css.buttonOutlinedSecondary, classes.buttonShadow)
-      }}
-      href={undefined}
-    >
-      {props.label}
-    </MUIButton>
-  );
-});
+export const Button = memo(
+  forwardRef<HTMLButtonElement, MUIButtonProps<'button', ButtonProps>>((props, ref) => {
+    const classes = useStyles();
+
+    return (
+      <MUIButton
+        {...props}
+        ref={ref}
+        data-qa={props['data-qa']}
+        onClick={props.onClick}
+        variant={props.variant}
+        color={props.color}
+        disabled={props.disabled}
+        className={clsx(props.customclasses)}
+        classes={{
+          root: css.button,
+          label: clsx(css.buttonLabel, props.customlabelclasses),
+          disabled: css.buttonDisabled,
+          containedPrimary: clsx(css.buttonPrimary, classes.buttonShadow),
+          containedSecondary: clsx(css.buttonSecondary, classes.buttonShadow),
+          outlined: clsx(css.buttonOutlined, classes.buttonShadow),
+          outlinedPrimary: clsx(css.buttonOutlined, classes.buttonShadow),
+          outlinedSecondary: clsx(css.buttonOutlinedSecondary, classes.buttonShadow)
+        }}
+        href={undefined}
+      >
+        {props.label}
+      </MUIButton>
+    );
+  })
+);
