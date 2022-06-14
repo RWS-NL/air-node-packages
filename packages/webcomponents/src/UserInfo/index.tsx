6345dff0f5ccb4340c96f69d800b2fa311c55a20
GvLit <gunnar.van.lit@rws.nl>
2022-06-14 14:16:07 +0200

    Added tooltip when hovering over users email

--- packages/webcomponents/src/UserInfo/index.tsx
+++ packages/webcomponents/src/UserInfo/index.tsx
@@ -2,10 +2,13 @@ import { Box, Button, ButtonProps, Typography, TypographyProps } from '@mui/mate
 import React, { memo, ReactNode } from 'react';
 import { When } from 'react-if';
 import css from './UserInfo.scss';
+import { Tooltip } from '@src/Tooltip';
 
 export interface UserInfoProps {
   /** The email to display as current user */
   email: string | ReactNode;
+  /** The external id of the current user */
+  externalId: string | ReactNode;
   /** The text to display for the relogin button */
   reloginText: ReactNode;
   /** Additional props to pass to the Typography component that renders the email */
@@ -22,23 +25,27 @@ export interface UserInfoProps {
  * @param param Props to pass to the UserInfo tooltip
  * @example
  * ```jsx
- * <UserInfo email='a.cool.email' reloginText='relogin' onReloginClick={() => console.log('void')} />
+ * <UserInfo email='a.cool.email' externalId='123456' reloginText='relogin' onReloginClick={() => console.log('void')} />
  * ```
  */
-export const UserInfo = memo(({ email, onReloginClick, reloginText, EmailProps, ButtonProps }: UserInfoProps) => (
-  <Box component='div' className={css.authInfo}>
-    <Box component='div' data-qa='logged-in-email'>
-      <When condition={email !== undefined}>
-        <Typography {...EmailProps} color='textPrimary' variant='subtitle2' align='center'>
-          {email}
-        </Typography>
-      </When>
-    </Box>
+export const UserInfo = memo(
+  ({ email, externalId, onReloginClick, reloginText, EmailProps, ButtonProps }: UserInfoProps) => (
+    <Box component='div' className={css.authInfo}>
+      <Box component='div' data-qa='logged-in-email'>
+        <When condition={email !== undefined}>
+          <Typography {...EmailProps} color='textPrimary' variant='subtitle2' align='center'>
+            <Tooltip title='externalId' placement='top'>
+              {email}
+            </Tooltip>
+          </Typography>
+        </When>
+      </Box>
 
-    <Button {...ButtonProps} data-qa='re-login-link' size='small' className={css.relogin} onClick={onReloginClick}>
-      <Typography color='textPrimary' variant='subtitle2' align='center'>
-        {reloginText}
-      </Typography>
-    </Button>
-  </Box>
-));
+      <Button {...ButtonProps} data-qa='re-login-link' size='small' className={css.relogin} onClick={onReloginClick}>
+        <Typography color='textPrimary' variant='subtitle2' align='center'>
+          {reloginText}
+        </Typography>
+      </Button>
+    </Box>
+  )
+);
